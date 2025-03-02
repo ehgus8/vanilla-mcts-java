31ed251f5070019844716ebe5f79364a6cfc6396
ldh <68594605+ehgus8@users.noreply.github.com>
2025-03-02 20:39:23 +0900

    refactor: remove duplicated code

--- src/ai/mcts/MCTS.java
+++ src/ai/mcts/MCTS.java
@@ -48,28 +48,25 @@ public class MCTS {
 
         int currentPlayer = node.currentPlayer;
         int moveCount = node.moveCount;
-
+        int winner = 0;
         int stateDim = game.rows * game.cols;
         List<Point> actionHistory = new ArrayList<>();
-        while(moveCount < stateDim) {
+        while(moveCount < stateDim && winner == 0) {
             List<Point> validActions = game.getValidActions();
             int actionIdx = (int)(Math.random() * validActions.size());
             Point action = validActions.get(actionIdx);
             currentPlayer = game.applyAction(currentPlayer, action);
             actionHistory.add(action);
             moveCount++;
-//            game.displayBoard();
-//            System.out.println("moveCount:"+moveCount);
-            int winner = game.checkWinner(currentPlayer * -1, action);
-            if(winner != 0) {
-                for(Point a: actionHistory) {
-                    game.undoAction(a);
-                }
-                return winner == (node.currentPlayer * -1) ? 1 : -1;
-            }
+
+            winner = game.checkWinner(currentPlayer * -1, action);
+
         }
         for(Point a: actionHistory) {
             game.undoAction(a);
+        }
+        if(winner != 0) {
+            return winner == (node.currentPlayer * -1) ? 1 : -1;
         }
         return 0; // draw
     }
